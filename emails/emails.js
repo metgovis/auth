cc754c8069c5d8fba9313d2f2d80fd9be2818302
Kevin <bonkewitzzkevin@gmail.com>
2022-10-24 15:49:23 +0200

    Welcome Email Fix

--- emails/emails.js
+++ emails/emails.js
@@ -44,7 +44,7 @@ exports.verify = (args) => {
 exports.welcome = (args) => {
     var deferred = Q.defer();
 
-    const transporter = nodemailer.createTransport(__settings.smtp);
+    const transporter = nodemailer.createTransport(JSON.parse(__settings.smtp));
 
     transporter.use('compile', hbs({
         'viewEngine': {
